acbef9c583922a61bbdb27a0293fbb9fb0f157d7
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-10-04 20:24:21 +0530

    dataTypes

---
+++ basic/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treate all js code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log( 3 + 3 )
+console.log('arsh')
+
+
+let name = "Arsh"
+let age = 21
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigInt 
+// string = "alkds"
+// boolean = true/false
+// null = standalone value and also it is an object
+// undefined = 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof(ihui))

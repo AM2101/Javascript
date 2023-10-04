d410f3344b8e4e5651a172a8d5c7af125b24102f
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-10-04 22:00:47 +0530

    conversion

---
+++ basic/conversionOperation.js
@@ -0,0 +1,46 @@
+// let score = 33 
+// let score = "33abc" 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score);
+// console.log(typeof (valueNumber));
+// console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 ; false => 0
+
+// let isLoggedIn = 1 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "arsh" => true
+
+// let someNumber = -2
+
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+// ****************** Operation ********************* //
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hello "
+// let str2 = "arsh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log(typeof("1" + 2));
+
+
+
+//************************************************************ *//
+
+//Stack (primitive), Heap (Non-Primitive)

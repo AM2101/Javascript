9a93ca6809ac6935c873374f2bfa463c8ea90599
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-10-04 20:09:46 +0530

    variables

---
+++ basic/variable.js
@@ -0,0 +1,7 @@
+const accountId= 51651;
+let accountName="arsh"
+var accountpassword="123456"
+accountCity= "mzn" //can be access
+let accountState; //ouput undefine
+
+console.table([accountId, accountName, accountpassword, accountState, accountCity])

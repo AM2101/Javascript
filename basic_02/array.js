b596f1562e0b8276c780ad466aee915caaa1aa9c
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-10-06 22:06:06 +0530

    array1

---
+++ basic_02/array.js
@@ -0,0 +1,42 @@
+
+
+const myArr = [ 0, 1, 2, 3, 4, 5 ]
+// myArr.forEach(function (item) {
+//     console.log('this is item: ' + item);
+// })
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(6)
+// myArr.pop(6)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+// console.log(myArr)
+
+// const newArr = myArr.join();
+// console.log(typeof newArr);
+// console.log(myArr)
+
+
+
+
+// slice, splice
+
+// console.log("A ", myArr)
+// const myn1 = myArr.slice(0,3)
+
+// console.log(myn1);
+// console.log("B ",myArr);
+
+// const myn2 = myArr.splice(0,2)
+// console.log("c ",myArr);
+// console.log(myn2);
+
+
+
+// 

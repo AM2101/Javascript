c76d1eadc1affa145782e3f456419ea724de8fd1
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-10-20 12:11:51 +0530

    flow

--- basic_03/iife.js
+++ basic_03/iife.js
@@ -1,4 +1,5 @@
 // Immediately Invoked function Expressions (IIFE)
+
 // some times gloabal variable ke pollucations ki vjha se problem ho jati hai to us provblem ko override krne ke liye IIFE use krte hai
 // and jo function phle execute krna hota hai uske liye bhi 
 
---
+++ control_flow_04/one.js
@@ -0,0 +1,17 @@
+// if statement
+
+// if (true) {
+
+// }else{
+
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==  
+
+
+// short hand notation
+
+// const balance = 1000
+
+// if(balance>100) console.log('test');
+

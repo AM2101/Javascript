46e95290b481b41bc5b43845829e6aca59610330
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-10-20 20:44:04 +0530

    for loop

---
+++ iterations_05/one.js
@@ -0,0 +1,23 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+
+// }
+
+// console.log("end");
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`table of ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//        console.log(`${i} * ${j} = ${i*j}`); 
+//     }
+//     console.log('\n');
+// }
+
+
+// break and continue

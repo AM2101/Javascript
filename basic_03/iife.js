533dc12b70cd395c037755c4f3c60c5803d135be
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-10-20 11:25:54 +0530

    iife

---
+++ basic_03/iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked function Expressions (IIFE)
+// some times gloabal variable ke pollucations ki vjha se problem ho jati hai to us provblem ko override krne ke liye IIFE use krte hai
+// and jo function phle execute krna hota hai uske liye bhi 
+
+(function chai(){
+    // named IIFE
+    console.log("DB connected");
+})();
+
+(  (name)=>{
+    // simple IIFE
+    console.log(`DB connected 2 ${name}`);
+}  )('arsh')
+

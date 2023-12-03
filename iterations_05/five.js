1ccff3d983340f9ff671e02727b9d22080d64e0c
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-12-03 23:12:40 +0530

    filter maps

--- iterations_05/five.js
+++ iterations_05/five.js
@@ -1,27 +1,31 @@
 // const coding = ["js", "ruby", "java", "python", "cpp"]
 
+// **************forEach loop does not return values *********************
 
 // coding.forEach( (item)=>{
 //     console.log(item);
 // } )
 
 
+
+//********** filter loop does return vlaues *********************** 
+
 const myNum = [1,2,3,4,5,6,7,8,9,10]
 
-// const values = myNum.filter ( (num)=>( num > 4 ) )
-
+const values = myNum.filter ( (num)=>( num > 4 ) )
+// console.log(values)
 // or 
 
 // const values = myNum.filter ( (num)=> {
 //     return num > 4
 // }  )
 
-const newNums = []
+// const newNums = []
 
-myNum.forEach((num) => {
-    if (num > 4)
-    newNums.push(num);
-})
+// myNum.forEach((num) => {
+//     if (num > 4)
+//     newNums.push(num);
+// })
 
 
-console.log(newNums);
+// console.log(newNums);
